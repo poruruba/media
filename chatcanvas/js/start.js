68641bffabfd76a59c7332e7b8afd71bd50a678c
poruruba <44470781+poruruba@users.noreply.github.com>
2022-03-05 15:59:21 +0900

    Add files via upload

---
+++ chatcanvas/js/start.js
@@ -0,0 +1,154 @@
+'use strict';
+
+//const vConsole = new VConsole();
+//window.datgui = new dat.GUI();
+
+const base_url = "https://home.poruru.work:20443";
+const room_name = "マイホーム";
+
+const UPDATE_INTERVAL = 5000;
+const icon_list = [
+    "boy_01.png",
+    "boy_02.png",
+    "boy_03.png",
+    "girl_13.png",
+    "girl_14.png",
+    "girl_15.png",
+    "man_49.png",
+    "man_50.png",
+    "man_51.png",
+    "oldman_73.png",
+    "oldman_74.png",
+    "oldman_75.png",
+    "oldwoman_85.png",
+    "oldwoman_86.png",
+    "oldwoman_87.png",
+    "woman_61.png",
+    "woman_62.png",
+    "woman_63.png",
+    "youngman_25.png",
+    "youngman_26.png",
+    "youngman_27.png",
+    "youngwoman_37.png",
+    "youngwoman_38.png",
+    "youngwoman_39.png",
+];
+
+var vue_options = {
+    el: "#top",
+    mixins: [mixins_bootstrap],
+    data: {
+        icon_selecting: 0,
+        icon_mine: "",
+        icon_list: icon_list,
+        chat_list: [],
+        message: "",
+        margin: 0,
+        last_chat_time: 0,
+        connected: false,
+        room: room_name,
+    },
+    computed: {
+    },
+    methods: {
+        icon_prev: function(){
+            if( this.icon_selecting == 0 )
+                this.icon_selecting = this.icon_list.length - 1;
+            else
+                this.icon_selecting--;
+        },
+        icon_next: function(){
+            if( this.icon_selecting >= this.icon_list.length - 1 )
+                this.icon_selecting = 0;
+            else
+                this.icon_selecting++;
+        },
+        append_chat: async function(){
+            if( this.connected ){
+                window.interactiveCanvas.sendTextQuery("ねえねえ、" + this.message);
+            }else{
+                var json = await do_post(base_url + "/chatcanvas-put-chat", {
+                    room: this.room,
+                    character: this.icon_mine,
+                    message: this.message });
+                console.log(json);
+            }
+        },
+        character_select: async function(){
+            this.icon_mine = this.icon_list[this.icon_selecting];
+            window.interactiveCanvas.setCanvasState({
+                character: this.icon_mine,
+                room: this.room
+            });
+            this.dialog_close('#character_select_dialog');
+
+            var json = await do_post(base_url + "/chatcanvas-get-chat", { room: this.room, start: this.last_chat_time });
+            if( json.status == 'ok' ){
+                this.append_chat_list(json);
+            } 
+            setInterval(async () =>{
+                console.log('setInterval function called');
+                var json = await do_post(base_url + "/chatcanvas-get-chat", { room: this.room, start: this.last_chat_time });
+                if( json.status == 'ok' ){
+                    this.append_chat_list(json);
+                } 
+            }, UPDATE_INTERVAL);
+        },
+        append_chat_list: function(json){
+            this.last_chat_time = json.time;
+            if( json.list.length <= 0 )
+                return;
+
+            for( var i = 0 ; i < json.list.length ; i++ ){
+                var talk = json.list[i];
+                var date = new Date(talk.post_time);
+                if( talk.character == this.icon_mine ){
+                    this.chat_list.push({
+                        align: "right",
+                        message: talk.message,
+                        date: date.toLocaleString(),
+                    });
+                }else{
+                    this.chat_list.push({
+                        align: "left",
+                        message: talk.message,
+                        date: date.toLocaleString(),
+                        img_src: "img/" + talk.character
+                    });
+                }
+            }
+            this.$nextTick(() =>{
+                const el = document.getElementById('el');
+                el.scrollTo(0, el.scrollHeight);
+            });
+        },
+    },
+    created: function(){
+    },
+    mounted: function(){
+        proc_load();
+
+        window.interactiveCanvas.getHeaderHeightPx()
+        .then(height => {
+            console.log("getHeaderHeightPx:" + height);
+            this.margin = height;
+            this.dialog_open('#character_select_dialog');
+        });
+        const callbacks = {
+            onUpdate: (data) => {
+                console.log(data);
+                if( !this.connected ){
+                    this.connected = true;
+                }
+            }
+        };
+        window.interactiveCanvas.ready(callbacks);
+    }
+};
+vue_add_data(vue_options, { progress_title: '' }); // for progress-dialog
+vue_add_global_components(components_bootstrap);
+vue_add_global_components(components_utils);
+
+/* add additional components */
+  
+window.vue = new Vue( vue_options );
